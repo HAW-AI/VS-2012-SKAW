889f884aa7e6e8728fbdc39e367f28d52481b640
sebkro <sebastian.krome@haw-hamburg.de>
2012-10-28 10:43:21 +0100

    verwaltung zur clientverwaltung hinzugefuegt

---
+++ Aufgabe1/verwaltung.erl
@@ -0,0 +1,53 @@
+-module(verwaltung).
+-export([start/1]).
+
+
+start(ForgetAfter) ->
+	Pid = spawn(fun() -> loop(ForgetAfter,dict:new(),dict:new()) end),
+	Pid.
+
+
+loop(ForgetAfter ,Dict, TimerDict) ->
+	receive
+		{getNo, KeyPid, Sender} -> getNo(Dict,KeyPid, Sender),
+			loop(ForgetAfter, Dict, TimerDict);
+		{storeNo, KeyPid, No} -> {NewDict,NewTimerDict} = storeNo(KeyPid,No, Dict, TimerDict, ForgetAfter),
+			loop(ForgetAfter, NewDict, NewTimerDict);
+		{timeout,KeyPid} -> {NewDict, NewTimerDict} = delete(Dict, TimerDict, KeyPid),
+			loop(ForgetAfter, NewDict, NewTimerDict);
+		kill -> io:format("Client verwaltung tot\n")
+	end.
+
+getNo(Dict,Pid, Sender) ->
+	io:format("Gebe "++pid_to_list(Pid)++" Nummer aus\n"),
+	Value = dict:find(Pid,Dict),
+	Sender ! {Pid,Value}.
+
+storeNo(Pid, No, Dict, TimerDict, ForgetAfter) ->
+	io:format("Speichere Nummer: "++pid_to_list(Pid)++integer_to_list(No)++"\n"),
+	NewDict = dict:store(Pid,No,Dict),
+	killTimer(TimerDict,Pid),
+	{_,TimerPid} = timer:send_after(ForgetAfter, {timeout,Pid}),
+	NewTimerDict = updateTimer(TimerDict,TimerPid, Pid),
+	{NewDict,NewTimerDict}.
+
+killTimer(TimerDict,KeyPid) ->
+	killTimer_(dict:find(KeyPid,TimerDict)).
+
+killTimer_({ok,Value}) ->
+	io:format("KillTimer "),
+	timer:cancel(Value);
+killTimer_(_) ->
+	io:format("Konnte Timer nicht finden\n").
+
+updateTimer(TimerDict, TimerPid, KeyPid) ->
+	NewTimerDict = dict:store(KeyPid,TimerPid, TimerDict),
+	NewTimerDict.
+
+delete(Dict, TimerDict, Pid) ->
+	io:format("loesche: "++pid_to_list(Pid)++"\n"),
+	NewDict = dict:erase(Pid,Dict),
+	NewTimerDict = dict:erase(Pid,TimerDict),
+	{NewDict, NewTimerDict}.
+
+
