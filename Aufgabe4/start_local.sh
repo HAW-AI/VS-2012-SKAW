a86a138ced1bf7796dab6fe2d876db57d2f5d9db
Andreas Wimmer <andreaswimmer.aw@googlemail.com>
2013-01-11 20:03:34 +0100

    Added starter script

---
+++ Aufgabe4/start_local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# example usage: 15100 10 1 "225.10.1.2" wlan
+
+IF_IP=$(/sbin/ifconfig $5 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+echo "Using IP:" $IF_IP "for interface" $5
+CLASSPATH=`dirname $0`
+HOST_NO=${HOSTNAME##lab}
+erl -pa ebin/ -sname sender$3 -setcookie skaw -boot start_sasl -noshell -s controller start $1 $2 $3 $4 $IF_IP > $3.log &
+echo $! > pidlist
